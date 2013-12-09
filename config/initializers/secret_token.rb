1fb9d3da481866d511b964aa28bf59007ea54bc8
ncroma <ncroma@gmail.com>
2013-12-09 22:50:22 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coise::Application.config.secret_token = '21715b9cc6777af71363f5b00b12a4ce68a209376ce12d00c01b2be12c26069dabfc35da3344f33d9f0b038f0f5e4c46b7138f194598171adad9e99859b3be22'
